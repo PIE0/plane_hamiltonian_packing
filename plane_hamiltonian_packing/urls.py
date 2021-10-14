30a9826662cdf76ca35559cd3ba29607881d80c7
Parsa Abdollahi <parsa.abdollahi.pa@gmail.com>
2021-10-14 13:30:05 +0330

    Add plane app

--- plane_hamiltonian_packing/urls.py
+++ plane_hamiltonian_packing/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from plane.views import plane_serve
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('plane/', plane_serve)
 ]
