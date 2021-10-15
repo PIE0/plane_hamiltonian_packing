8bc636d6056e1aca9c660fae5c144255a2e86298
Parsa Abdollahi <parsa.abdollahi.pa@gmail.com>
2021-10-15 13:18:34 +0330

    Refactor cpp part a bit

--- cpp/run.sh
+++ cpp/run.sh
@@ -1,1 +1,7 @@
-g++ --std=c++17 main.cpp graph_utils.cpp && ./a.out input.txt - sum rnd_rot four_per
+# Compile code with this command
+g++ --std=c++17 main.cpp graph_utils.cpp
+# Example Runs
+./a.out batch gen 10 5 20
+./a.out single gen 20 300
+./a.out batch - 12 good_tests good_tests sum rnd_rot four_perm
+./a.out single - sum rnd_rot four_perm
